0356766c030a28fae1ab240f933964d1a57cb1b2
serbarla <31071618+serbarla@users.noreply.github.com>
2017-08-16 14:51:39 -0500

    Groupwork
    Software developed as a group, used to keep track of accounting and inventory management in an Internet Cafe

---
+++ Imprima/src/imprima/Imprima.java
@@ -0,0 +1,14 @@
+package imprima;
+
+public class Imprima {
+
+    public static void main(String[] args) {
+        Inicio a= new Inicio();
+        a.setSize(500,500);
+        a.setVisible(true);
+        a.pack();
+        a.move(350,250);
+    }
+    
+ 
+}
